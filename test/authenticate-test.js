b936bbec126618d47308b42d81404f7ea0e1556b
Qiu <qiu.gits@gmail.com>
2018-02-08 20:15:05 +0900

    More tests

--- test/authenticate-test.js
+++ test/authenticate-test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const getAuthenticate = require('../authenticate');
 
-it('Login test', () => {
+it('Login test: No auth', () => {
   process.env.UTID_13 = '';
   process.env.UTID_PASS = '';
   process.env.MANABA_USERNAME = '';
@@ -13,3 +13,26 @@ it('Login test', () => {
   assert(auth.username === '' && auth.password === '');
 });
 
+it('Login test: Manaba only', () => {
+  process.env.UTID_13 = '';
+  process.env.UTID_PASS = 'not enough';
+  process.env.MANABA_USERNAME = 'mn-n';
+  process.env.MANABA_PASSWORD = 'mn-p';
+
+  const auth = getAuthenticate(debug=false);
+  console.log(auth);
+
+  assert(auth.username === 'mn-n' && auth.password === 'mn-p');
+});
+
+it('Login test: UTID surpasses', () => {
+  process.env.UTID_13 = 'ut-n';
+  process.env.UTID_PASS = 'ut-p';
+  process.env.MANABA_USERNAME = 'aaa';
+  process.env.MANABA_PASSWORD = 'bbb';
+
+  const auth = getAuthenticate(debug=false);
+  console.log(auth);
+
+  assert(auth.username === 'ut-n' && auth.password === 'ut-p');
+});
